1e428eeaab7dabc7828f6a5931e801fe76207a0f
Ting <yitingwu.1030@gmail.com>
2022-11-08 19:04:50 +0800

    Initial Commit

---
+++ TodaySport/TodaySportApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodaySportApp.swift
+//  TodaySport
+//
+//  Created by Victor Tao on 2022/11/8.
+//
+
+import SwiftUI
+
+@main
+struct TodaySportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
